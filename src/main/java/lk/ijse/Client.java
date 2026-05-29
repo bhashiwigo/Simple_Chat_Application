923211ec62fc54574ac4a7b4c74b16db7f0f144d
Chamod Bhashitha Wijegunarathne <bhashiwigo@gmail.com>
2026-05-29 14:42:55 +0530

    Update 01

--- src/main/java/lk/ijse/Client.java
+++ src/main/java/lk/ijse/Client.java
@@ -1,4 +1,22 @@
 package lk.ijse;
 
-public class Client {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Client extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/lk/ijse/ClientForm.fxml"));
+        stage.setTitle("Cyber Chat - Client");
+        stage.setScene(new Scene(root));
+        stage.setResizable(false);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
--- src/main/java/lk/ijse/Server.java
+++ src/main/java/lk/ijse/Server.java
@@ -1,4 +1,22 @@
 package lk.ijse;
 
-public class Server {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Server extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/lk/ijse/ServerForm.fxml"));
+        stage.setTitle("Cyber Chat - Server");
+        stage.setScene(new Scene(root));
+        stage.setResizable(false);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
